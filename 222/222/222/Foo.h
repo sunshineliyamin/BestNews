4a2db62d8964eb8f3a6ce356d869de6e8fb602e6
2282196355 <2282196355@qq.com>
2015-05-22 09:38:48 +0800

    foo

---
+++ 222/222/222/Foo.h
@@ -0,0 +1,13 @@
+//
+//  Foo.h
+//  222
+//
+//  Created by lanou3g on 15/5/22.
+//  Copyright (c) 2015年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Foo : UIViewController
+
+@end
---
+++ 222/222/222/Foo.m
@@ -0,0 +1,13 @@
+//
+//  Foo.m
+//  222
+//
+//  Created by lanou3g on 15/5/22.
+//  Copyright (c) 2015年 lanou3g. All rights reserved.
+//
+
+#import "Foo.h"
+
+@implementation Foo
+
+@end
